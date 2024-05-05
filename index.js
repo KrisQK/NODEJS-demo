9839fa78d89499b5f37648a091eefa8920648190
Kris Qikun Li - Mac <qikunli8866+mac@gmail.com>
2024-05-05 12:21:46 +1200

    review the all nodejs

---
+++ index.js
@@ -0,0 +1,7 @@
+import getPosts, { getPostsLength } from "./postController.js";
+
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+// console.log(generateRandomNumber());
+// console.log(celciusToFahrenheit(30));
+console.log(getPosts());
+console.log(getPostsLength());
